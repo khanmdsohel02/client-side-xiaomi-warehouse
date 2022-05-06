5f5e9ef565b0d4177a876331415fad1ae9a2f811
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2022-05-07 01:51:26 +0600

    used post method to insert data

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -4,7 +4,21 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
      const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+        const url = 'http://localhost:5000/product';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+  }
     return (
         <>
             <h1 className='mt-5'> Add Item</h1>
