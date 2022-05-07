7f04402ca0d7fe807eb754908f47c0a6a7696507
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2022-05-08 05:51:49 +0600

    .env.locak file done

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { getAuth} from "firebase/auth";
 import {initializeApp} from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlqN6zf1klvjyUFieoM-Qv6lnIpuI2bEc",
-    authDomain: "xiaomi-warehouse.firebaseapp.com",
-    projectId: "xiaomi-warehouse",
-    storageBucket: "xiaomi-warehouse.appspot.com",
-    messagingSenderId: "894444372507",
-    appId: "1:894444372507:web:f464d011ccec71e8c52ebe"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
